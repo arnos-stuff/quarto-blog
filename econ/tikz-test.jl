6a20108532e5fff5304d8fae1a35034e26420283
arnos-stuff <bcda0276@gmail.com>
2023-02-10 13:25:03 +0100

    mass commit to port to wsl2 with cuda

---
+++ econ/tikz-test.jl
@@ -0,0 +1,39 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 246c3840-a567-11ed-2c4f-fb583275e10b
+using Pkg
+
+Pkg.add("PGFPlots, LaTeXStrings, Graphs, TikzGraphs")
+
+# ╔═╡ 0039ab4e-ce26-47d7-9285-3ea69f7c2a90
+
+
+# ╔═╡ 7a963466-7988-4611-a4eb-36048bc0629d
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.5"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═246c3840-a567-11ed-2c4f-fb583275e10b
+# ╠═0039ab4e-ce26-47d7-9285-3ea69f7c2a90
+# ╠═7a963466-7988-4611-a4eb-36048bc0629d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
